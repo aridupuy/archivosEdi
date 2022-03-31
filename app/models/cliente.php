708ba44611e9d9cb57c65c31be2f790dd8021bca
Ariel Dupuy <adupuy@cobrodigital.com>
2022-03-31 00:26:17 -0300

    modelos

---
+++ app/models/cliente.php
@@ -0,0 +1,72 @@
+<?php
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
+ */
+
+/**
+ * Description of cliente
+ *
+ * @author adupuy
+ */
+class Cliente extends Model{
+    //put your code here
+    public static $id_tabla = "id_cliente";
+    private $id_cliente;
+    private $nombre_completo;
+    private $email;
+    private $telefono;
+    private $documento;
+    
+    
+    public function get_id_cliente() {
+        return $this->id_cliente;
+    }
+
+    public function get_nombre_completo() {
+        return $this->nombre_completo;
+    }
+
+    public function get_email() {
+        return $this->email;
+    }
+
+    public function get_telefono() {
+        return $this->telefono;
+    }
+
+    public function get_documento() {
+        return $this->documento;
+    }
+
+    public function set_id_cliente($id_cliente) {
+        $this->id_cliente = $id_cliente;
+        return $this;
+    }
+
+    public function set_nombre_completo($nombre_completo) {
+        $this->nombre_completo = $nombre_completo;
+        return $this;
+    }
+
+    public function set_email($email) {
+        $this->email = $email;
+        return $this;
+    }
+
+    public function set_telefono($telefono) {
+        $this->telefono = $telefono;
+        return $this;
+    }
+
+    public function set_documento($documento) {
+        $this->documento = $documento;
+        return $this;
+    }
+
+
+    
+    
+    
+}
