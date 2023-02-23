2b6d8d5a6a7af830b8b79f9cf63f982756a22b09
adupuy <adupuy@cobrodigital.com>
2023-02-23 19:39:37 -0300

    wayback listados

--- app/Http/Controllers/Edisoft/ContainerController.php
+++ app/Http/Controllers/Edisoft/ContainerController.php
@@ -411,7 +411,7 @@ class ContainerController extends \App\Http\Controllers\Controller {
             $ie = new \Ie($row);
             $fecha_recepcion = $hora_recepcion = "";
             if($row["fecha_recepcion"]!=null )
-                $fecha_recepcion = \DateTime::createFromFormat("!Y-m-d", $row["fecha_recepcion"])->format("Y-m-d");
+                $fecha_recepcion = \DateTime::createFromFormat("Y-m-d", $row["fecha_recepcion"])->format("Y-m-d");
             if($fecha_recepcion==""){
                 $fecha_recepcion = \DateTime::createFromFormat("Y-m-d H:i:s", $row["fecha_gen"])->format("Y-m-d");
             }
