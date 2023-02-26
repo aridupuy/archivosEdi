32e343812c13de61589e1bc7ef62ad0f5499eba6
adupuy <adupuy@cobrodigital.com>
2023-02-26 19:34:17 -0300

    email

--- app/Http/Controllers/Edisoft/UsuarioController.php
+++ app/Http/Controllers/Edisoft/UsuarioController.php
@@ -176,6 +176,7 @@ class UsuarioController extends \App\Http\Controllers\Controller {
     public function recuperar_pass($request) {
         $rs = \Usuario::select(array("email" => self::$variables["email"]));
         $usuario = new \Usuario($rs->fetchRow());
+        var_dump($usuario->get_id_usuario());
         if ($usuario->get_id_usuario()==null) {
             $rs = \Usuario::select(array("nombre_usuario" => self::$variables["email"]));
             $usuario = new \Usuario($rs->fetchRow());
@@ -184,6 +185,7 @@ class UsuarioController extends \App\Http\Controllers\Controller {
             }
         }
         $id_usuario = $usuario->get_id_usuario();
+        var_dump($usuario->get_id_usuario());
         if (\Gestor_de_correo::enviar_con_adjunto(\Gestor_de_correo::MAIL_INFO, self::$variables["email"],"Cambio de contraseña", "Para cambiar la contraseña haz click en <a href=\"https://www.transportesrossell.com/admin/#/change-password?key={$id_usuario}\">Recuperar clave</a>",null)) {
             $resp = self::RESPUESTA_CORRECTA;
             $response["msg"] = "Correo enviado.";
