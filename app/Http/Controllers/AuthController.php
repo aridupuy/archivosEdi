b2c59cfbd98972204cff512782130cba370878cc
adupuy <adupuy@cobrodigital.com>
2023-03-06 21:30:21 -0300

    password fix

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -181,7 +181,7 @@ class AuthController extends Log_in_app {
         $usuario->get($variables["id"]);
         $usuario->actualizar_password();
         $pass = $usuario->get_password();
-        $usuario->set_password(isset($variables["newPass"])?$variables["newPass"]:$variables["clave"]);
+        $usuario->set_password(isset($variables["password"])? $variables["password"]: $variables["clave"]);
         dd([$pass, $usuario->get_password()]);
         if($usuario->set()){
             $response = json_encode(["check" => true,"cuenta"=> $usuario]);
